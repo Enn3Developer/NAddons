ab1df329ca28356e37c50bcefa7d5057218b9b9a
enn3 <arturpeshko39@gmail.com>
2024-01-26 15:26:21 +0100

    removed normal energy sensor support

--- src/main/java/com/enn3developer/naddons/NAddonsCrossMod.java
+++ src/main/java/com/enn3developer/naddons/NAddonsCrossMod.java
@@ -8,19 +8,6 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 
 public class NAddonsCrossMod extends CrossModBase {
     @Override
-    public CompoundTag getEnergyData(BlockEntity te) {
-        if (!(te instanceof EnergyCounterTile energyCounter)) {
-            return null;
-        }
-
-        CompoundTag tag = new CompoundTag();
-        tag.putString(DataHelper.EUTYPE, "EU");
-        tag.putDouble(DataHelper.ENERGY, energyCounter.getCountedEU());
-        tag.putDouble(DataHelper.CAPACITY, energyCounter.getAddedPerTick());
-        return tag;
-    }
-
-    @Override
     public CompoundTag getCardData(BlockEntity te) {
         if (!(te instanceof EnergyCounterTile energyCounter)) {
             return null;
