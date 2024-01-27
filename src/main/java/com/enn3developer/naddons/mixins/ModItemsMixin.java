a5cab59b8f0115efd1984c4fcfb8190125bd81dd
enn3 <arturpeshko39@gmail.com>
2024-01-28 00:32:47 +0100

    better mixins for tan

--- src/main/java/com/enn3developer/naddons/mixins/ModItemsMixin.java
+++ src/main/java/com/enn3developer/naddons/mixins/ModItemsMixin.java
@@ -3,18 +3,25 @@ package com.enn3developer.naddons.mixins;
 import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import toughasnails.init.ModItems;
+import toughasnails.item.DirtyWaterBottleItem;
+import toughasnails.item.PurifiedWaterBottleItem;
+import toughasnails.util.inventory.ItemGroupTAN;
 
 @Mixin(ModItems.class)
 public class ModItemsMixin {
-    @Redirect(method = "lambda$registerItems$4", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item$Properties;stacksTo(I)Lnet/minecraft/world/item/Item$Properties;"), remap = false)
-    private static Item.Properties dirtyWaterBottle(Item.Properties instance, int pMaxStackSize) {
-        return instance.stacksTo(64);
+    @Inject(method = "lambda$registerItems$4", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private static void injectDirtyWaterBottle(CallbackInfoReturnable<Item> cir) {
+        cir.setReturnValue(
+                new DirtyWaterBottleItem((new Item.Properties()).stacksTo(64).tab(ItemGroupTAN.INSTANCE)));
     }
 
-    @Redirect(method = "lambda$registerItems$5", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item$Properties;stacksTo(I)Lnet/minecraft/world/item/Item$Properties;"), remap = false)
-    private static Item.Properties purifiedWaterBottle(Item.Properties instance, int pMaxStackSize) {
-        return instance.stacksTo(64);
+    @Inject(method = "lambda$registerItems$5", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private static void injectPurifiedWaterBottle(CallbackInfoReturnable<Item> cir) {
+        cir.setReturnValue(
+                new PurifiedWaterBottleItem((new Item.Properties()).stacksTo(64).tab(ItemGroupTAN.INSTANCE)));
     }
 }
