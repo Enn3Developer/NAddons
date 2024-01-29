f0af2dbdec48e0fa505e4cc1927ed2677b14175d
enn3 <arturpeshko39@gmail.com>
2024-01-29 15:07:47 +0100

    better ui

--- src/main/java/com/enn3developer/naddons/screens/EnergyCounterScreen.java
+++ src/main/java/com/enn3developer/naddons/screens/EnergyCounterScreen.java
@@ -4,12 +4,12 @@ import com.enn3developer.naddons.NAddons;
 import com.enn3developer.naddons.menus.EnergyCounterMenu;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.gui.components.PlainTextButton;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraftforge.client.gui.widget.ExtendedButton;
 import org.jetbrains.annotations.NotNull;
 
 public class EnergyCounterScreen extends AbstractContainerScreen<EnergyCounterMenu> {
@@ -33,14 +33,14 @@ public class EnergyCounterScreen extends AbstractContainerScreen<EnergyCounterMe
         RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
         RenderSystem.setShaderTexture(0, TEXTURE);
         this.blit(poseStack, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
-        float drawX = this.leftPos + ((float) this.imageWidth / 10) + 30;
-        float drawY = this.topPos + ((float) this.imageHeight / 10) - 5;
+        float drawX = this.leftPos + ((float) this.imageWidth / 3);
+        float drawY = this.topPos + ((float) this.imageHeight / 10);
         String energy = (this.menu.getEnergyCounter() != null ? this.menu.getEnergyCounter().getCountedEU() : 0) + " Wh";
         String power = (this.menu.getEnergyCounter() != null ? this.menu.getEnergyCounter().getPower() : 0) + " kW";
         this.font.draw(poseStack, energy, drawX, drawY, 12);
         this.font.draw(poseStack, power, drawX, drawY + 10, 12);
-        this.addRenderableWidget(new PlainTextButton((int) drawX, (int) (drawY + 30), 20, 20, Component.literal("Reset"), button -> {
+        this.addRenderableWidget(new ExtendedButton((int) drawX, (int) (drawY + 25), 50, 16, Component.literal("Reset"), button -> {
 
-        }, this.font));
+        }));
     }
 }
