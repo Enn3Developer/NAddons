6c5f72063f7cf5100bec5ce573cc72e0df8548f7
enn3 <arturpeshko39@gmail.com>
2024-05-06 14:56:24 +0200

    rebalanced tan

--- src/main/java/com/enn3developer/naddons/mixins/ModItemsMixin.java
+++ src/main/java/com/enn3developer/naddons/mixins/ModItemsMixin.java
@@ -7,20 +7,42 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import toughasnails.init.ModItems;
 import toughasnails.item.DirtyWaterBottleItem;
+import toughasnails.item.FilledCanteenItem;
 import toughasnails.item.PurifiedWaterBottleItem;
 import toughasnails.util.inventory.ItemGroupTAN;
 
 @Mixin(ModItems.class)
 public class ModItemsMixin {
+    @Inject(method = "lambda$registerItems$1", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private static void injectDirtyWaterCanteen(CallbackInfoReturnable<Item> cir) {
+        cir.setReturnValue(
+                new FilledCanteenItem((new Item.Properties()).durability(16).tab(ItemGroupTAN.INSTANCE))
+        );
+    }
+
+    @Inject(method = "lambda$registerItems$2", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private static void injectWaterCanteen(CallbackInfoReturnable<Item> cir) {
+        cir.setReturnValue(
+                new FilledCanteenItem((new Item.Properties()).durability(16).tab(ItemGroupTAN.INSTANCE))
+        );
+    }
+
+    @Inject(method = "lambda$registerItems$3", at = @At(value = "HEAD"), remap = false, cancellable = true)
+    private static void injectPurifiedWaterCanteen(CallbackInfoReturnable<Item> cir) {
+        cir.setReturnValue(
+                new FilledCanteenItem((new Item.Properties()).durability(16).tab(ItemGroupTAN.INSTANCE))
+        );
+    }
+
     @Inject(method = "lambda$registerItems$4", at = @At(value = "HEAD"), remap = false, cancellable = true)
     private static void injectDirtyWaterBottle(CallbackInfoReturnable<Item> cir) {
         cir.setReturnValue(
-                new DirtyWaterBottleItem((new Item.Properties()).stacksTo(64).tab(ItemGroupTAN.INSTANCE)));
+                new DirtyWaterBottleItem((new Item.Properties()).stacksTo(8).tab(ItemGroupTAN.INSTANCE)));
     }
 
     @Inject(method = "lambda$registerItems$5", at = @At(value = "HEAD"), remap = false, cancellable = true)
     private static void injectPurifiedWaterBottle(CallbackInfoReturnable<Item> cir) {
         cir.setReturnValue(
-                new PurifiedWaterBottleItem((new Item.Properties()).stacksTo(64).tab(ItemGroupTAN.INSTANCE)));
+                new PurifiedWaterBottleItem((new Item.Properties()).stacksTo(8).tab(ItemGroupTAN.INSTANCE)));
     }
 }
